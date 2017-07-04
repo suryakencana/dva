95110d3abe038b3dedaf626c21a6d02f013bca20
Nanang Suryadi <nanang.jobs@gmail.com>
2017-07-04 15:14:52 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import { createBrowserHistory } from 'history/createBrowserHistory';
 import { routerReducer as routing, routerMiddleware, syncHistoryWithStore } from 'react-router-redux';
 import createDva from './createDva';
 
-const customHistory = createBrowserHistory();
-
 export default createDva({
   mobile: false,
   initialReducer: {
     routing,
   },
-  defaultHistory: customHistory,
+  defaultHistory: createBrowserHistory,
   routerMiddleware,
 
   setupHistory(history) {
