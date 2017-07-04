7bac2e7d30a0251e00ad85048ac000fc598d8ff3
Nanang Suryadi <nanang.jobs@gmail.com>
2017-07-04 21:06:05 +0700

    chore(package): reset to original

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import createHistory from 'history/createBrowserHistory';
-import { routerReducer as routing, routerMiddleware } from 'react-router-redux';
+import createHashHistory from 'history/createBrowserHistory';
+import {
+  routerMiddleware,
+  syncHistoryWithStore,
+  routerReducer as routing,
+} from 'react-router-redux';
 import createDva from './createDva';
-
 
 export default createDva({
   mobile: false,
   initialReducer: {
     routing,
   },
-  defaultHistory: createHistory(),
+  defaultHistory: createHashHistory,
   routerMiddleware,
 
   setupHistory(history) {
-    this._history = history;
+    this._history = syncHistoryWithStore(history, this._store);
   },
 });
